5ec32aa01bd0dcf489cba4774a3a7252c25b9f0b
nagarjunreddykasu <nagarjun.sdet@gmail.com>
2021-02-02 08:04:36 +0530

    online

---
+++ OnlineTraining/src/assignments/Assignment1.java
@@ -0,0 +1,10 @@
+package assignments;
+
+public class Assignment1 {
+
+}
+
+
+//nagarjun.sdet@gmail.com
+
+
---
+++ OnlineTraining/src/assignments/Assignment2.java
@@ -0,0 +1,8 @@
+package assignments;
+
+
+//define methods with all primitive types as return type and call those methods inside main method
+
+public class Assignment2 {
+
+}
