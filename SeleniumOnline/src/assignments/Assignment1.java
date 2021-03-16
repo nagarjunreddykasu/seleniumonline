68f4549ba713b01fef6b79e9b67ab609d1625777
nagarjunreddykasu <nagarjun.sdet@gmail.com>
2021-03-16 09:33:29 +0530

    online

---
+++ SeleniumOnline/src/assignments/Assignment1.java
@@ -0,0 +1,42 @@
+package assignments;
+
+public class Assignment1 {
+
+}
+
+
+/*
+
+1. Write the code to verify all the expected fields/buttons/links are displaying in 'Sign Up' page.
+Steps:
+Open http://qa.circulus.io/login.aspx site
+Navigate to 'Sign Up' page
+
+2. Write the code to verify missing mandatory validation messages in Registration page.
+Steps:
+Open http://qa.circulus.io/login.aspx site
+Navigate to 'Sign Up' page
+Click on 'Register' button and verify all the validation messages
+
+
+3. Write the code to select any value from 'How did you hear about us?' drop down list.
+Steps:
+Open http://qa.circulus.io/login.aspx site
+Navigate to 'Sign Up' page
+Select a value from 'How did you hear about us?' drop down list.
+
+4. Write the code to compare actual values from 'How did you hear about us?' drop down list with expected values.
+Steps:
+Open http://qa.circulus.io/login.aspx site
+Navigate to 'Sign Up' page
+
+5. Write the code to select departure city value and arrival city value from respective drop downs.
+Steps:
+Open https://www.spicejet.com/ site
+Select departure city value and arrival city value
+
+
+
+
+
+*/
